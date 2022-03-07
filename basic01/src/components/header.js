48bfa672db4ccad2748dbfdabaff7220b557cad2
teshalecaleb <teshalecaleb@gmail.com>
2022-03-07 18:50:39 +0300

    first lesson

---
+++ basic01/src/components/header.js
@@ -0,0 +1,29 @@
+import React , {Component}from "react";
+
+// const Header = () =>{
+//     const getTheYear = () => {
+//         const newDate = new Date();
+//         return newDate.getFullYear();
+//     }
+//     return(
+//         <div>
+//             The date is {Date.now()}
+//         </div>
+//     )
+
+    
+// }
+class Header extends Component{
+    render(){
+        return(
+            <header>
+            <div>Logo</div>
+            <input/>
+            </header>
+
+        )
+
+        
+    }
+}
+export default Header;
---
+++ basic01/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import Header from './components/header';
+
+const App = () => (
+    // <React.Fragment>
+    //     <h1>Hello</h1>
+    //     <h2>Hello Kaleb</h2>
+    // </React.Fragment>
+    <>
+    <Header/>
+    </>
+
+)
+
+
+
+ 
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
